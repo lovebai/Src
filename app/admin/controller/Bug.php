2bfd079a411d9456d369448abca3ef5e8a4f0912
Administrator <admin@xiaobaibk.cn>
2021-05-18 23:00:15 +0800

    新增

--- app/admin/controller/Bug.php
+++ app/admin/controller/Bug.php
@@ -20,7 +20,11 @@ class Bug extends Base
     public function list(){
         $limit=Request::post("limit");
         if($limit!=''){
-            $bugList=Bugb::name('bug')->order('gid','desc')->paginate($limit);
+            $bugList=Bugb::name('bug')->order('gid','desc')->paginate($limit)->each(function ($item){
+                $date=Bugb::name('bugcg')->where('id',$item['type'])->find();
+                $item['type']=$date['category'];
+                return $item;
+            });
         }else{
             return $this->create_return(false,201,'error',0,'json');
         }
@@ -52,16 +56,16 @@ class Bug extends Base
     }
 
     public function category(){
-//        if(!empty(Request::param())&&Request::isAjax()&&Request::has('limit')){
-        if(!empty(Request::param())&&Request::has('limit')){
+        if(!empty(Request::param())&&Request::isAjax()&&Request::has('limit')){
             $limit=Request::post("limit");
             if($limit!=''){
                 $list=Bugb::name('bugcg')->paginate($limit)->each(function ($item,$key){
-//
-//                    $aa=Bugb::name('bugcg')->where('u_id',$item['u_id'])->where('e_id',$item['u_id'])->find();
-//                    $item['test']=$aa;
-//
-//                    return $item['test'];
+                    if($item['is']=='y'){
+                        $item['category']="<span id='wy' style=\"color:#FF5722;font-weight: 700;letter-spacing: 5px;\">".$item['category']."</span>";
+                    }else{
+                        $item['categorys']=$item['category'];
+                    }
+                    return $item;
                 });
             }else{
                 return $this->create_return(false,201,'error',0,'json');
@@ -74,10 +78,11 @@ class Bug extends Base
         }
     }
 
+    //添加分类
     public function addcategory(){
         if(Request::isAjax()&&Request::has('category')){
             $data=Request::param();
-
+            $data['is']= $data['u_id']==0?'y':'n';
             if(Bugb::name('bugcg')->insert($data)){
                 return $this->create_return(true,200,'恭喜您分类添加成功了',1,'json');
             }else{
@@ -90,5 +95,48 @@ class Bug extends Base
             ]);
         }
     }
+
+    //编辑分类
+    public function editcategory(){
+        if(Request::get('edit_id')&&Request::has('edit_id')){
+            $data=Bugb::name('bugcg')->where('id',Request::get('edit_id'))->find();
+            if($data){
+                return View::fetch('category_edit',[
+                    'data'=>$data
+                ]);
+            }else{
+                return "error";
+            }
+        }else if(!empty(Request::param())&&Request::isAjax()&&Request::has('category')){
+            $data=Request::param();
+            if(Request::has('is')){
+                $data['is']='y';
+            }else{
+                $data['is']='n';
+            }
+            if(Bugb::name('bugcg')->where('id',$data['id'])->save($data)){
+                return $this->create_return(true,200,'已编辑成功',1,'json');
+            }else{
+                return $this->create_return(false,201,'编辑失败，可能是您未做过修改',0,'json');
+            }
+        }else{
+            return $this->create_return(false,400,'error',0,'json');
+        }
+    }
+
+    //删除分类
+    public function delcategory(){
+        $id=Request::post('id');
+        if($id!=''&&!empty($id)&&Request::isAjax()&&Request::has('id')){
+            if(Bugb::name('bugcg')->where('id',$id)->delete()){
+                return $this->create_return([],200,'恭喜您删除成功！',1,'json');
+            }else{
+                return $this->create_return([],203,'删除失败！',0,'json');
+            }
+        }else{
+            return $this->create_return([],201,'提交参数有误！',0,'json');
+        }
+    }
+
 
 }
