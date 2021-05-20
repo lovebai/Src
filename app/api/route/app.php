9bba48f5afd26d71d1b996e405c051aa36c59808
Administrator <admin@xiaobaibk.cn>
2021-05-20 23:13:51 +0800

    update

--- app/api/route/app.php
+++ app/api/route/app.php
@@ -6,3 +6,14 @@ Route::group('thread',function (){
     Route::rule('','post/articleList');
     Route::rule('id','post/article');
 });
+
+//用户
+Route::group('user',function (){
+    Route::rule('','user/index');
+});
+
+
+//漏洞提交
+Route::group('submit',function (){
+    Route::rule('','bug/index');
+});
