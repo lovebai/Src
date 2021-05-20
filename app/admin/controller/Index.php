72dc4460f5a593fd6d99c5c99b30a0165f0a560d
Administrator <admin@xiaobaibk.cn>
2021-05-20 20:59:52 +0800

    add file

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -3,6 +3,8 @@ declare (strict_types = 1);
 
 namespace app\admin\controller;
 
+use think\facade\Db;
+use think\facade\Session;
 use think\facade\View;
 
 class Index extends Base
@@ -11,6 +13,42 @@ class Index extends Base
     public function index()
     {
         return View::fetch("/index");
+    }
+
+    public function avatar(){
+        if(Session::has('USER_ID')){
+            $id=Session::get('USER_ID');
+            $info=Db::name('admin')->where('id',$id)->find();
+            if($info){
+                if ($info['avatar']!=''){
+                    $avatar=$info['avatar'];
+                }else{
+                    if ($info['qq']!='') {
+                        $avatar = '//api.xiaobaibk.com/api/qq.php?qq=' . $info['qq'];
+                    }else{
+                        $avatar='//tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg';
+                    }
+                }
+            }
+
+        }
+        return json(array('avatar'=>$avatar,'user'=>$info['username']));
+
+
+    }
+
+    public function preference(){
+        if(Session::has('USER_ID')){
+            $id=Session::get('USER_ID');
+            $info=Db::name('admin')->where('id',$id)->find();
+            if($info){
+                $info['password']='';
+                return View::fetch('/preference',[
+                    'info'=>$info
+                ]);
+            }
+
+        }
     }
 
 }
